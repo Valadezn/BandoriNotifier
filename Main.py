b9a3ed907a46e487cd16e79fbb74e363cf328210
Emi <valadezn@uci.edu>
2020-03-11 00:32:42 -0700

    Added html search to find event title and timestamp

--- Main.py
+++ Main.py
@@ -7,24 +7,31 @@ def sendToast(message):
 	toaster = ToastNotifier()
 	toaster.show_toast("Sample Notification",message)
 
-# def getTimeStamp(language = "EN"):
-# 	bestdori_url = "http://bestdori.com"
-
-# 	req = Request(bestdori_url, headers={'User-Agent': 'Mozilla/5.0'})
-# 	webpage = urlopen(req).read()
-# 	soup = BeautifulSoup(webpage, "lxml")
-# 	print(soup.prettify())
 
 def getTimeStamp(language ="EN"):
 	bestdori_url = "http://bestdori.com"
 	session  = HTMLSession()
 	resp = session.get(bestdori_url)
 	resp.html.render(sleep=2)
-	print(resp.html.text)
 
-	#soup = BeautifulSoup(resp.html.html, "lxml")
-	#print(soup.prettify())
-	# print()
+	event_name_elements = resp.html.find(".title.is-5")
+	timestamp_elements = resp.html.find(".subtitle.is-6")
+
+	event_titles = []
+	timestamp_texts = []
+
+	skip_texts = {"Bestdori! - The Ultimate BanG Dream GBP Resource Site", "News", "Support Us", "Support Bestdori!"}
+
+	for el in event_name_elements:
+		if el.text not in skip_texts:
+			event_titles.append(el.text)
+	print("____")
+	for el in timestamp_elements:
+		if el.text not in skip_texts:
+			timestamp_texts.append(el.text)
+
+	for i in range(len(event_titles)):
+		print("{} - {}".format(event_titles[i], timestamp_texts[i]))
 
 if __name__ == "__main__":
 	getTimeStamp()
